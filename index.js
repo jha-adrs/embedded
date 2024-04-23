6f712c49b81ec25e4d503a2f88184fa7f4b8ad2e
Aadarsh <aadarshjha6783@gmail.com>
2024-04-23 14:37:15 +0530

    Minor log changes

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ app.post("/createHousehold", async (req, res) => {
 
 const PORT = process.env.NODE_PORT || 3000;
 app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port ',PORT);
     db.init();
 });
